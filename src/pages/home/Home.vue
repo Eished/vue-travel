cea06a8f952639c78886873c8011f620b44d3d5a
Eished <kished@outlook.com>
2020-02-04 22:47:24 +0800

    增加Footer模块

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,6 +5,7 @@
     <home-icons></home-icons>
     <home-recommend></home-recommend>
     <home-hot></home-hot>
+    <home-footer></home-footer>
   </div>
 </template>
 
@@ -14,6 +15,8 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeHot from './components/Hot'
+import HomeFooter from './components/Footer'
+
 export default {
   name: 'Home',
   components: {
@@ -21,7 +24,8 @@ export default {
     HomeSwiper,
     HomeIcons,
     HomeRecommend,
-    HomeHot
+    HomeHot,
+    HomeFooter
   }
 }
 </script>
---
+++ src/pages/home/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="footer">
+    <p class="copyright">
+      <span>Copyright ©2019 asdf.com</span>
+      <a href="#">京公网安备1101asdf30542</a>
+      <a href="#">京ICP备asdf7号</a>
+      <a href="#">营业执照信息</a>
+      <a href="#">保险经纪资质</a>
+    </p>
+    <p class="copyright">
+      <span>违法和不良信息举报电话：010-69194277</span>
+      <span>违法和不良信息举报邮箱：tousu@qunar.com</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'qnFooter'
+}
+</script>
+
+<style lang="stylus" scoped>
+.footer
+  width 90%
+  margin 2rem auto 0 auto
+  .copyright
+    margin 0.2rem auto
+    text-align center
+  a, p
+    margin 0.1rem
+</style>
