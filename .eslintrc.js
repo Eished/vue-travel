a5d138566889957058581114ba1afbfe8a2c9361
Eished <kished@outlook.com>
2021-08-14 23:46:15 +0800

    vue基础部分

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   env: {
     browser: true,
@@ -13,28 +13,39 @@ module.exports = {
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'standard',
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
 
+    'comma-dangle': ['error', 'only-multiline'],
+    // never（默认）不允许尾随逗号
+    // always 需要尾随逗号
+    // "only-multiline"允许（但不要求）, 后面跟括号则不允许
+    // "always-multiline" 后面跟括号也需要
+
     // anonymous 针对匿名函数表达式 (比如 function () {})。
     // named 针对命名的函数表达式 (比如 function foo () {})。
     // asyncArrow 针对异步的箭头函数表达式 (比如 async () => {})。
-    "space-before-function-paren": ["error", {
-      "anonymous": "always",
-      "named": "ignore",
-      "asyncArrow": "always"
-    }],
-
-
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'ignore',
+        named: 'ignore',
+        asyncArrow: 'always',
+      },
+    ],
+    'no-new': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+  globals: {
+    document: true,
+    window: true,
+    Vue: true,
+  },
 }
